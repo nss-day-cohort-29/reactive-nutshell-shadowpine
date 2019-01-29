fd365c4366f2d28c05aba9e6aafc7db89602bb0a
Brittany Ramos-Janeway <bramosjaneway@gmail.com>
2019-01-29 12:22:55 -0600

    added comments

--- src/components/articles/ArticlesBoard.js
+++ src/components/articles/ArticlesBoard.js
@@ -5,13 +5,15 @@ export default class ArticlesBoard extends Component {
     render(){
         return (
             <React.Fragment>
-            <h1>Articles</h1>
-            <button class="article--new">Add Article</button>
-            <section className="articles">
-                {this.props.articles.map(article => (
-                    <ArticlesCard key={article.id} article={article} {...this.props} />
-                ))}
-            </section>
+                {/* Create title, new button, and articles container */}
+                <h1>Articles</h1>
+                <button class="article--new">Add Article</button>
+                <section className="articles">
+                    {/* add each article to the DOM */}
+                    {this.props.articles.map(article => (
+                        <ArticlesCard key={article.id} article={article} {...this.props} />
+                    ))}
+                </section>
             </React.Fragment>
         )}
 }
