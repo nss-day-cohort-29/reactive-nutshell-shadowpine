1305a97196123b6653e3ac5d2191664372c8f3d6
Brittany Ramos-Janeway <bramosjaneway@gmail.com>
2019-01-29 15:41:17 -0600

    Changess

--- src/components/articles/ArticlesForm.js
+++ src/components/articles/ArticlesForm.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
 
 export default class ArticlesForm extends Component {
-    render() {
-      return <h1>FORM</h1>
+    state = {
+        hideForm: false
+    };
+
+    handleChange = () => {
+        this.setState= ({hideForm: true});
     }
+
+    render() {
+      return (
+        <React.Fragment>
+            <label>
+                Title
+                <input />
+            </label>
+            <label>
+                Synopsis
+                <textarea></textarea>
+            </label>
+            <label>
+                URL
+                <input />
+            </label>
+            <button>Submit</button>
+        </React.Fragment>
+      )}
 }
