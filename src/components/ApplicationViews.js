f6a628f56f2d9ec85aae7121b959224ac2fd3a1c
Brittany Ramos-Janeway <bramosjaneway@gmail.com>
2019-01-29 12:58:10 -0600

    added post function

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -31,8 +31,30 @@ export default class ApplicationViews extends Component {
     this.postNewMessage = this.postNewMessage.bind(this);
   }
 
+// *********************************ARTICLES******************************************
+addArticle(articleObject){
+  ArticlesManager.post(articleObject)
+    .then(() => ArticlesManager.getAll())
+    .then(articles =>
+      this.setState({
+        articles: articles
+      })
+  );
+}
 
 
+// *********************************MESSAGES******************************************
+
+postNewMessage(messageObj) {
+  MessagesManager.post(messageObj)
+  .then(() => MessagesManager.getAll()
+  .then(messages => this.setState({
+    messages: messages
+  })));
+}
+
+
+// *********************************TASKS******************************************
   deleteTask = id => {
     return fetch(`http://localhost:5002/tasks/${id}`, {
       method: "DELETE"
@@ -58,7 +80,12 @@ export default class ApplicationViews extends Component {
 
 
   componentDidMount() {
-
+    ArticlesManager.getAll()
+      .then(allArticles => {
+        this.setState({
+          articles: allArticles
+        });
+      });
     TasksManager.getAll().then(allTasks => {
       this.setState({
         tasks: allTasks
@@ -69,29 +96,8 @@ export default class ApplicationViews extends Component {
     .then(messages => this.setState({
       messages: messages
     }));
-
   }
 
-  postNewMessage(messageObj) {
-    MessagesManager.post(messageObj)
-    .then(() => MessagesManager.getAll()
-    .then(messages => this.setState({
-      messages: messages
-    })));
-  }
-
-  componentDidMount() {
-    MessagesManager.getAll()
-      .then(messages => this.setState({
-        messages: messages
-      }));
-    ArticlesManager.getAll()
-      .then(allArticles => {
-        this.setState({
-          articles: allArticles
-        });
-      });
-  }
 
   render() {
     console.log(this.state)
--- src/modules/ArticlesManager.js
+++ src/modules/ArticlesManager.js
@@ -3,5 +3,14 @@ const remoteUrl = "http://localhost:5002";
 export default {
     getAll() {
         return fetch(`${remoteUrl}/articles`).then(r => r.json());
+    },
+    post(newArticle) {
+        return fetch(`${remoteUrl}/articles`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newArticle)
+        }).then(data => data.json())
     }
-}
+};
