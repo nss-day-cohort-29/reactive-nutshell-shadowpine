236f09c603e49ac862cd18131841298d914b8289
JD <jdwheeler0@gmail.com>
2019-01-30 16:31:48 -0600

    adding branch

--- src/components/tasks/TasksEdit.js
+++ src/components/tasks/TasksEdit.js
@@ -1,30 +1,69 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
 
-// export default class TasksForm extends Component {
-//     state = {
-//         tasksToEdit: ""
-//     }
+export default class TasksForm extends Component {
+    state = {
+        tasksToEdit: ""
+    }
 
-//     handlefieldChange = event => {
-//         const stateToChange = {};
-//         stateToChange.editedContent = event.target.value;
-//         this.setState(stateToChange);
-//     }
+    handlefieldChange = event => {
+        const stateToChange = {};
+        stateToChange.editedContent = event.target.value;
+        this.setState(stateToChange);
+    }
 
-//     createEditObject = event => {
-//         const editedTasks = {
-//             id: this.props.tasks.id,
-//             "tasksTitle":this.props.tasks.tasksTitle ,
-//             "expectedCompletionDate":this.props.tasks.expectedCompletionDate ,
-//             "complete": this.props.tasks.complete
-//         };
-//         console.log(editedTasks);
-//         this.props.handleSaveButton(editedTasks, event);
-//     }
+    createEditObject = event => {
+        const editedTasks = {
+            id: this.props.tasks.id,
+            "tasksTitle":this.props.tasks.tasksTitle ,
+            "expectedCompletionDate":this.props.tasks.expectedCompletionDate ,
+            "complete": this.props.tasks.complete
+        };
+        console.log(editedTasks);
+        this.props.handleSaveButton(editedTasks, event);
+    }
 
 
-//     render() {
+    render() {
+        render() {
+            return (
+              <React.Fragment>
+                <form className="editTasksForm">
+                  <div className="form-group">
+                    <label htmlFor="editTasksTitle">Task Title</label>
+                    <input
+                      type="text"
+                      required
+                      className="form-control"
+                      onChange={this.handleFieldChange}
+                      id="editTasksTitle"
+                      placeholder="Edit Task Title"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="editeddExpectedCompletionDate"> Estimated Completion Date </label>
+                    <input
+                      type="date"
+                      required
+                      className="form-control"
+                      onChange={this.handleFieldChange}
+                      id="editedExpectedCompletionDate"
+                      placeholder="Edit Completion Date"
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    onClick={this.constructNewTask}
+                    className="btn btn-primary"
+                  >
+                    Create New
+                  </button>
+                </form>
 
-//         )
-//     }
-// }
+              </React.Fragment>
+            );
+          }
+        }
+
+
+
+}
